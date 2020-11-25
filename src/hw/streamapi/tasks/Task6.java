dcc982b67e816aebee0b0ef2e261d51d37f4aa57
anykuzya <anyakuz48@gmail.com>
2020-11-25 22:55:10 +0300

    task 6 done (finally!)

--- src/hw/streamapi/tasks/Task6.java
+++ src/hw/streamapi/tasks/Task6.java
@@ -7,7 +7,6 @@ import hw.streamapi.common.Task;
 import java.time.Instant;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /*
@@ -17,12 +16,14 @@ import java.util.stream.Collectors;
 - коллекция всех регионов Collection<Area>
 На выходе хочется получить множество строк вида "Имя - регион". Если у персон регионов несколько, таких строк так же будет несколько
  */
+
 public class Task6 implements Task {
 
     // после первой задачки кажется, что здесь можно применить такой же подход с мапами из id в объекты
     // сейчас по памяти это O(n+m) где n -- кол-во персон, m -- кол-во областей,
     // а по времени вроде бы тоже линия (за которую мы создаем маппинги из id в объекты)
-    // но пока это нечитаемо, а еще скорее всего сломается на тёзках
+    private static final String separator = " - ";
+
     private Set<String> getPersonDescriptions(Collection<Person> persons,
                                               Map<Integer, Set<Integer>> personAreaIds,
                                               Collection<Area> areas) {
@@ -31,18 +32,24 @@ public class Task6 implements Task {
             .collect(Collectors.toMap(Person::getId, Function.identity()));
         Map<Integer, Area> idsToAreas = areas.stream()
             .collect(Collectors.toMap(Area::getId, Function.identity()));
-        var namesToAreas = personAreaIds.keySet().stream()
-            .collect(Collectors.toMap(id -> idsToPersons.get(id).getFirstName(), // duplicateKeyException for second Vasya            // это мы зафиксировали, что в этой мапе будут имена людей
-                id -> personAreaIds.get(id).stream().map(area_id -> idsToAreas.get(area_id).getName()).collect(Collectors.toSet()))); // это мы коллекцию областей каждого юзера получили в виде строк (и прочитать это сейчас невозможно)
-        return namesToAreas.keySet().stream().flatMap(name -> namesToAreas.get(name).stream().map(area -> name + " - " + area)).collect(Collectors.toSet());
+
+        return personAreaIds.entrySet().stream() // это стрим с айдишниками юзеров
+            .flatMap(entry -> {
+                var userId = entry.getKey();
+                return entry.getValue().stream() // а это маленький стрим для каждого юзера, в нем его (этого юзера) области
+                    .map(areaId ->
+                        idsToPersons.get(userId).getFirstName()
+                            + separator
+                            + idsToAreas.get(areaId).getName()); // и это тоже все ещё стрим, но уже стрим со строками заданного формата для этого юзера
+            }).collect(Collectors.toSet());
     }
 
     @Override
     public boolean check() {
         List<Person> persons = List.of(
             new Person(0, "Oleg", Instant.now()),
-            new Person(1, "Vasya", Instant.now()),
-            new Person(2, "Vasya", Instant.now())
+            new Person(1, "Vasya", "Ivanov", Instant.now()),
+            new Person(2, "Vasya", "Petrov", Instant.now())
         );
         Map<Integer, Set<Integer>> personAreaIds = Map.of(0, Set.of(1, 2), 1, Set.of(2, 3), 2, Set.of(1));
         List<Area> areas = List.of(new Area(1, "Moscow"), new Area(2, "Spb"), new Area(3, "Ivanovo"));
