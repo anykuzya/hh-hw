0b999ea697b012381a82de42ea12854a56be0c63
anykuzya <anyakuz48@gmail.com>
2021-02-15 18:00:53 +0300

    rework repo structure and add servers hw template

---
+++ coding-homeworks/src/hw/servers/jersey/src/main/java/JerseyApplication.java
@@ -0,0 +1,8 @@
+package hw.servers.jersey.src.main.java;
+
+public class JerseyApplication {
+
+  public static void main(String[] args) {
+    // run, Jetty, run!
+  }
+}
---
+++ coding-homeworks/src/hw/servers/servlet/src/main/java/ServletApplication.java
@@ -0,0 +1,8 @@
+package hw.servers.servlet.src.main.java-java-servers-homework;
+
+public class ServletApplication {
+
+  public static void main(String[] args) {
+    // run, Jetty, run!
+  }
+}
